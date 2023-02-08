ecff286279b98a75b4cc7d02d8bf2b987b1ff298
ckevin <kevin.chaffaux@ynov.com>
2023-02-08 20:32:45 +0100

    :sparkles: | add Relation & location API

--- api.go
+++ api.go
@@ -5,9 +5,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
-type artists struct {
+type Artists []struct {
 	Id           int
 	Image        string
 	Name         string
@@ -19,21 +20,110 @@ type artists struct {
 	Relations    string
 }
 
-func TestAPI() []byte { //Fonction pour récupérer les infos sur l'artiste/groupe
+type Locations struct {
+	Index []struct {
+		ID        int
+		Locations []string
+	}
+}
+
+type Dates struct {
+	Index []struct {
+		ID    int
+		Dates []string
+	}
+}
+
+type Relation struct {
+	Index []struct {
+		ID             int64
+		DatesLocations map[string][]string
+	}
+}
+
+/*---------------------- Artist API ----------------------*/
+
+func GetArtist() []byte { //Fonction pour récupérer les infos sur l'artiste/groupe
 	url := "https://groupietrackers.herokuapp.com/api/artists"
 	req, _ := http.NewRequest("GET", url, nil)
-	res, _ := http.DefaultClient.Do(req)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 	return body // renvoie un tableau de bytes
 }
 
-func UnMarshall(data []byte) { // Fonction pour UnMarshall le résultat de la requête API
-	var tab []artists
+func UnMarshallArtists(data []byte) Artists { // Fonction pour UnMarshall le résultat de la requête API Artiste
+	var tab Artists
 	err := json.Unmarshal([]byte(data), &tab)
 	if err != nil {
-		fmt.Println("Error", err)
-		return
+		fmt.Println(err)
+		os.Exit(1)
 	}
-	fmt.Println(tab) // Pour l'instant il est simplement print (dans le futur il seras renvoyé pour la page HTML)
+	fmt.Println(tab[0])
+	return tab // Pour l'instant il est simplement print (dans le futur il seras renvoyé pour la page HTML)
 }
+
+/*---------------------- Artist API ----------------------*/
+
+/*---------------------- Location API ----------------------*/
+
+func GetLocation() []byte {
+	url := "https://groupietrackers.herokuapp.com/api/locations"
+	req, _ := http.NewRequest("GET", url, nil)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return body
+}
+
+func UnMarshallLocations(data []byte) Locations { // Fonction pour UnMarshall le résultat de la requête API Location
+	var tab Locations
+	json.Unmarshal(data, &tab)
+	fmt.Println(tab)
+	return tab
+}
+
+/*---------------------- Location API ----------------------*/
+
+/*---------------------- Relation API ----------------------*/
+
+func GetRelation() []byte { //Fonction pour récupérer les infos sur l'artiste/groupe
+	url := "https://groupietrackers.herokuapp.com/api/relation"
+	req, _ := http.NewRequest("GET", url, nil)
+	res, err := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return body // renvoie un tableau de bytes
+}
+
+func UnMarshallRelation(data []byte) Relation { // Fonction pour UnMarshall le résultat de la requête API Artiste
+	var tab Relation
+	json.Unmarshal(data, &tab)
+	fmt.Println(tab)
+	return tab
+}
+
+/*---------------------- Relation API ----------------------*/
